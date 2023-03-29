4ed53fb11c9fe80c585cbefaf29502229e0af149
nicobritto <nicobritto21@gmail.com>
2023-03-29 14:12:52 -0300

    ejercicio terminado sin usar for

---
+++ contar.sh
@@ -0,0 +1,12 @@
+
+sdf1=$(wc -l <  loremipsum-1.txt)
+sdf2=$(wc -l <  loremipsum-2.txt)
+sdf3=$(wc -l <  loremipsum-3.txt)
+sdf4=$(wc -l <  loremipsum-4.txt)
+sdf5=$(wc -l <  loremipsum-5.txt)
+
+echo loremipsum-1.txt tiene "$sdf1"
+echo loremipsum-2.txt tiene "$sdf2"
+echo loremipsum-3.txt tiene "$sdf3"
+echo loremipsum-4.txt tiene "$sdf4"
+echo loremipsum-5.txt tiene "$sdf5"
